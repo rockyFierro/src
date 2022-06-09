b98e2486c8ac9e7e9c3aff9b10400447988a801b
rockyFierro <finnslighthouse@gmail.com>
2022-06-08 23:43:54 -0700

    java example

---
+++ com/arfcode/SimpleEx.java
@@ -0,0 +1,12 @@
+package com.arfcode;
+// The package name must correspond to the directory structure
+// in which the source file is located
+
+public class SimpleEx
+// The public class name must match the file name
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is the Java example. Be impressed!");
+    }
+}
